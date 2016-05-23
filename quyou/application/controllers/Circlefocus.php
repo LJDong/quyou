b93e9008fe6795e02dec446ae06378ea31acd091
jidong <jidong@jidong>
2016-05-23 23:43:01 +0800

    搜索

---
+++ quyou/application/controllers/Circlefocus.php
@@ -0,0 +1,53 @@
+<?php
+class circlefocus extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("url_helper");
+        $this->load->library("session");
+        $this->load->model("circlefocusmodel");
+    }
+    public function getcirclelist()
+    {
+        $user_id = $_SESSION['user_id'];
+        if(isset($user_id))
+        {
+            $this->circlefocusmodel->select(array('cf_user_id',$user_id));
+            $this->jsonreturn("查询成功", $ret, 0);
+            exit;
+        }
+        else
+       {
+            $this->jsonreturn("未登录", array(), -1);
+        }
+    }
+    public function focuscircle()
+    {
+        $circle_id = $_POST['circle_id'];
+        if(isset($_SESSION["user_id"]))
+        {
+            $arr = array(
+                'cf_circle_id'=>$circle_id,
+                'cf_user_id'=>$_SESSION["user_id"],
+                'cf_time'=>time()
+            );
+            $ret = $this->circlefocusmodel->insert($arr);
+            $this->jsonreturn("查询成功", $ret, 0);
+            exit();
+        }
+        else 
+       {
+            $this->jsonreturn("未登录", array(), -1);
+            exit;
+        }
+    }
+    public function jsonreturn($reason,$result,$error_code)
+    {
+        $jsonret = json_encode(array(
+            'reason'=>$reason,
+            'result'=>$result,
+            'error_code'=>$error_code
+        ));
+        echo $jsonret;
+    }
+}
